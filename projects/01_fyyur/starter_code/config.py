e913921a73427475fc4d0284d3f589e9333e5c0d
CALEB <CALEB@DESKTOP-EA7DEJ6>
2022-08-21 22:59:00 +0000

    latest commit

--- projects/01_fyyur/starter_code/config.py
+++ projects/01_fyyur/starter_code/config.py
@@ -10,4 +10,4 @@ DEBUG = True
 
 
 # TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = '<Put your local database url>'
+SQLALCHEMY_DATABASE_URI = 'postgresql://ct:1worldwelivein@localhost:5432/Fyurr'
